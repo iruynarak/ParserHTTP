f997fd22b8fe8de75f6593f6dfd551293687b202
iruynarak <iruynarak@gmail.com>
2014-10-17 08:27:41 -0400

    Ampliação da RequestHeader
    Na classe RequestHeader
    
    * Criação dos parâmetros "method", "requestURI" e "messageBody".
    * Mudança no construtor.
    * Criação do método getMessageBody

--- RequestHeader.h
+++ RequestHeader.h
@@ -1,26 +1,46 @@
-
-//http://www.cplusplus.com/reference/map/map/
-//http://kengine.sourceforge.net/tutorial/g/stdmap-eng.htm
-
-#include <map>
-#include<string>
-
-
 #ifndef _RequestHeader
 #define _RequestHeader
 #endif
+
+//http://www.cplusplus.com/reference/map/map/
+//http://kengine.sourceforge.net/tutorial/g/stdmap-eng.htm
+#include <map>
+
+//std::string
+#include <string>
 
 typedef std::map< std::string , std::string> Map;
 
 class RequestHeader
 {
 	public:
+
+		/// Para esta implementação o método pode ser GET ou POST
+		std::string method;
+
+		///
+		std::string requestURI;
+
+		/// Aonde serão guardados os parâmetros do request através de uma relação chave-valor(parâmetro-valor)
 		Map headerFields;
 
-		RequestHeader();
+		///
+		std::string messageBody;
+
+		/// Construtor
+		RequestHeader(std::string method, std::string requestURI)
+		{
+			this->requestURI = requestURI;
+			this->method =  method;
+		}
 
 		void insert(std::string key, std::string value)
 		{
 			headerFields.insert(key, value);
+		}
+
+		void getMessageBody(std::string messageBody)
+		{
+			this->messageBody = messageBody;
 		}
 };
